30266f0f23e747f2f1def0f1324c13cf39e234f7
AlexSychev01 <alex39ksg@mail.ru>
2020-05-13 08:55:37 +0200

    hello world appl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //https://github.com/AlexSychev01/Hello-world.git
+        //Упраженение #1
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+        System.out.println("Hello world!");
+
+        //Упраженение #2
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        System.out.println("Hello world!");
+
+        //Упраженение #3
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("sum = " + c);
+
+        //Упраженение #4
+        int a1 = 1;
+        int b1 = 2;
+        System.out.println("a1 > b1 = " + (a1 > b1));
+        System.out.println("a1 < b1 = " + (a1 < b1));
+
+        //Упраженение #5
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+
+
     }
 }
